19d4cb2319f611feda1ca5ac8a45208818f0fcc6
kisaragim <keito@techmatrix.co.jp>
2023-05-02 09:02:01 +0900

    provide project

---
+++ DevelopSampleProject/cpptestcli_exec.sh
@@ -0,0 +1,14 @@
+export CPPTEST_DIR=/opt/app/cpptest
+export PATH=${CPPTEST_DIR}:$PATH
+
+export WORKSPACE_DIR=../
+export PROJECT_NAME=DevelopSampleProject
+export PROJECT_DIR=$WORKSPACE_DIR$PROJECT_NAME
+
+cpptestcli \
+-data $WORKSPACE_DIR \
+-resource $PROJECT_NAME \
+-config "$WORKSPACE_DIR/tools/TestConfig/1_フロー解析（チュートリアル）.properties" \
+-report $PROJECT_DIR/report \
+-showdetails \
+-appconsole stdout
---
+++ DevelopSampleProject/generate_bdf.sh
@@ -0,0 +1,13 @@
+# パス設定
+export CPPTEST_TRACE_DIR=/home/keio/cpptest/bin
+export PATH=${PATH}:${CPPTEST_TRACE_DIR}
+# ビルドデータファイル作成のための環境変数設定
+export PROJECT_DIR=$PWD
+export CPPTEST_SCAN_OUTPUT_FILE=${PROJECT_DIR}/cpptestscan.bdf
+export CPPTEST_SCAN_PROJECT_NAME=DevelopSampleProject
+# export CPPTEST_SCAN_TRACE_COMMAND=gcc^|g\+\+
+# ビルドデータファイル作成
+cd ${PROJECT_DIR}
+rm -f ${PROJECT_DIR}/cpptestscan.bdf
+make clern
+cpptesttrace make -i -B
